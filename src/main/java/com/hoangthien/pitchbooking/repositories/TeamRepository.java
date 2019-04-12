26ac4fd3fe89d3afcddfa5e3c0d148f62e4b5cf9
Thien Nguyen <Thien.Hoang.Nguyen@mgm-tp.com>
2019-04-13 00:08:13 +0700

    Adding some configs

--- src/main/java/com/hoangthien/pitchbooking/repositories/TeamRepository.java
+++ src/main/java/com/hoangthien/pitchbooking/repositories/TeamRepository.java
@@ -1,11 +1,10 @@
 package com.hoangthien.pitchbooking.repositories;
 
 import com.hoangthien.pitchbooking.entities.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TeamRepository {
+public interface TeamRepository extends JpaRepository<Team, Long> {
     boolean existsByPath(String path);
-
-    Team save(Team team);
 }
